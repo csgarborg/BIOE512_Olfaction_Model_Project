2e452b786e44c00fe27d1a2a39b596fa25f41555
csg178 <csg178@ESM-10967.coeaccess.psu.edu>
2018-11-14 15:04:51 -0500

    Added file with simple model equations, not working yet

---
+++ SimpleAC3Model.m
@@ -0,0 +1,5 @@
+dcAMP = (2*.63*CNG) - (2*.08*cAMP^2*(.74-CNG)) - (3.16*cAMP) - (47.02*cAMP*CaCaM) + u;
+dCNG = (.08*cAMP^2*(.74-CNG)) - (.63*CNG) - (163.17*CNG*CaBP^2);
+dCa = (47.29*CNG) - (3.32*Ca) - (.84*Ca*(.74-CaBP)) + (.6*CaBP) - (2*.01*Ca^2*(1.3-CaCaM)) + (2*.1*CaCaM);
+dCaBP = (.84*Ca*(.74-CaBP)) - (.6*CaBP);
+dCaCaM = (.01*Ca^2*(1.3-CaCaM)) - (.1*CaCaM);
