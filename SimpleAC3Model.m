742b241b668533ae799a2603b94b0c84b5ea22de
csg178 <csg178@ESM-10967.coeaccess.psu.edu>
2018-11-14 16:15:47 -0500

    first draft, got figures for 5 concentration parameters, need to figure out initial conditions

--- SimpleAC3Model.m
+++ SimpleAC3Model.m
@@ -1,5 +1,44 @@
-dcAMP = (2*.63*CNG) - (2*.08*cAMP^2*(.74-CNG)) - (3.16*cAMP) - (47.02*cAMP*CaCaM) + u;
-dCNG = (.08*cAMP^2*(.74-CNG)) - (.63*CNG) - (163.17*CNG*CaBP^2);
-dCa = (47.29*CNG) - (3.32*Ca) - (.84*Ca*(.74-CaBP)) + (.6*CaBP) - (2*.01*Ca^2*(1.3-CaCaM)) + (2*.1*CaCaM);
-dCaBP = (.84*Ca*(.74-CaBP)) - (.6*CaBP);
-dCaCaM = (.01*Ca^2*(1.3-CaCaM)) - (.1*CaCaM);
+clear all
+cAMP(1) = .1*10^-6;
+CNG(1) = .1*10^-6;
+Ca(1) = .1*10^-6;
+CaBP(1) = .1*10^-6;
+CaCaM(1) = .1*10^-6;
+dt = .01;
+
+for t = 1:5000
+    if 10 <= t && t <= 3000
+        u = 200;
+    else
+        u = 0;
+    end
+    
+    dcAMP = (2*.63*CNG(t)) - (2*.08*cAMP(t)^2*(.74-CNG(t))) - (3.16*cAMP(t)) - (47.02*cAMP(t)*CaCaM(t)) + u;
+    dCNG = (.08*cAMP(t)^2*(.74-CNG(t))) - (.63*CNG(t)) - (163.17*CNG(t)*CaBP(t)^2);
+    dCa = (47.29*CNG(t)) - (3.32*Ca(t)) - (.84*Ca(t)*(.74-CaBP(t))) + (.6*CaBP(t)) - (2*.01*Ca(t)^2*(1.3-CaCaM(t))) + (2*.1*CaCaM(t));
+    dCaBP = (.84*Ca(t)*(.74-CaBP(t))) - (.6*CaBP(t));
+    dCaCaM = (.01*Ca(t)^2*(1.3-CaCaM(t))) - (.1*CaCaM(t));
+    
+    cAMP(t+1) = cAMP(t) + dcAMP*dt;
+    CNG(t+1) = CNG(t) + dCNG*dt;
+    Ca(t+1) = Ca(t) + dCa*dt;
+    CaBP(t+1) = CaBP(t) + dCaBP*dt;
+    CaCaM(t+1) = CaCaM(t) + dCaCaM*dt;
+    
+    
+end
+
+subplot(5,1,1)
+plot(0:.01:50,cAMP)
+
+subplot(5,1,2)
+plot(0:.01:50,CNG)
+
+subplot(5,1,3)
+plot(0:.01:50,Ca)
+
+subplot(5,1,4)
+plot(0:.01:50,CaBP)
+
+subplot(5,1,5)
+plot(0:.01:50,CaCaM)
